5ec5b7fc4ae29b2a31425221bc201aa86f20e655
DigitalForge1 <MrGamerTop1@gmail.com>
2024-12-19 12:53:58 +0200

    first commit

---
+++ Default.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="SluzbaControl.SluzbaControl" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Поиск по проекту Служба Контроля</title>
+    <link rel="stylesheet" href="Styles.css"/>
+    <style type="text/css">
+        .tdTB {
+            width: 70%;
+            padding-right: 15px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="body">
+            <asp:TextBox ID="phoneTB" runat="server" BackColor="White" BorderColor="White" BorderStyle="None" ForeColor="#CCFFFF" ReadOnly="True" Width="100px"></asp:TextBox>
+            <div class="search">
+                <asp:Label ID="Label1" runat="server" Font-Size="Large" ForeColor="#333333" Text="Введите ключевое слово:"></asp:Label>
+                <br />
+                <table style="width:100%;">
+                    <tr>
+                        <td class="tdTB">
+                            <asp:TextBox ID="TextBox1" CssClass="TB" runat="server" Font-Size="Medium" Width="100%"></asp:TextBox>
+                        </td>
+                        <td>
+                            <asp:Button ID="Button1" CssClass="searchBTN" runat="server" Font-Size="Medium" Text="Найти" Width="100%" OnClick="Button1_Click" />
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <asp:GridView ID="GridView1" CssClass="mydatagrid" PagerStyle-CssClass="pager" HeaderStyle-CssClass="header" RowStyle-CssClass="rows" runat="server" AutoGenerateColumns="False">
+            <Columns>
+                <asp:BoundField DataField="Phone" HeaderText="Номер телефона">
+                    <ItemStyle HorizontalAlign="Center" VerticalAlign="Middle" />
+                </asp:BoundField>
+                <asp:BoundField DataField="Dolznost" HeaderText="Должность">
+                    <ItemStyle HorizontalAlign="Center" VerticalAlign="Middle" />
+                </asp:BoundField>
+            </Columns>
+                <HeaderStyle CssClass="header" />
+                <PagerStyle CssClass="pager" />
+                <RowStyle CssClass="rows" />
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+private DataTable createBase()
+{
+    string connectionString = "Data Source=192.168.0.97;Initial Catalog=oktell;Persist Security Info=True;User ID=root;Password=web";
+    string zapros = "SELECT id, Phone, Dolznost FROM BaseSluzbaControl ORDER BY Dolznost";
+    DataTable dt = new DataTable();
+    using (SqlConnection con = new SqlConnection(connectionString))
+    {
+        using (SqlCommand cmd = new SqlCommand(zapros))
+        {
+            using (SqlDataAdapter sda = new SqlDataAdapter())
+            {
+                cmd.Connection = con;
+                sda.SelectCommand = cmd;
+                sda.Fill(dt);
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+        }
+    }
+    return dt;
+}
+
+protected void select_OnClick_linkBtn(object sender, EventArgs e)
+{
+    LinkButton lnkbtn = sender as LinkButton;
+    GridViewRow gvrow = lnkbtn.NamingContainer as GridViewRow;
+
+    string connectionString = "Data Source=192.168.0.97;Initial Catalog=oktell;Persist Security Info=True;User ID=root;Password=web";
+    string zapros = $"SELECT Phone FROM BaseSluzbaControl WHERE id = '{GridView1.Rows[gvrow.RowIndex].Cells[0].Text}'";
+
+    using (SqlConnection con = new SqlConnection(connectionString))
+    {
+        using (SqlCommand cmd = new SqlCommand(zapros, con))
+        {
+            con.Open();
+            SqlDataReader sqlDataReader = cmd.ExecuteReader();
+            if (sqlDataReader.Read())
+            {
+                phoneTB.Text = sqlDataReader["Phone"].ToString();
+            }
+            sqlDataReader.Close();
+        }
+    }
+}
+
+protected void Button2_Click(object sender, EventArgs e)
+{
+    GridViewRow row = (GridViewRow)((Button)sender).Parent.Parent;
+    string forwardingNumber = row.Cells[1].Text; // Phone
+    var chainid = HttpContext.Current.Request.QueryString["Chainid"];
+
+    if (!string.IsNullOrEmpty(forwardingNumber))
+        SetFil(forwardingNumber, chainid);
+}
+
+private void SetFil(string forwardingNumber, string Chainid)
+{
+    string connectionString = "Data Source=192.168.0.97;Initial Catalog=oktell;Persist Security Info=True;User ID=root;Password=web";
+    string zapros = $"UPDATE vh_SluzhbaKontrolya SET Phone='{forwardingNumber}' WHERE id='{Chainid}'";
+
+    using (SqlConnection con = new SqlConnection(connectionString))
+    {
+        using (SqlCommand cmd = new SqlCommand(zapros, con))
+        {
+            con.Open();
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
+
+protected void Button1_Click(object sender, EventArgs e)
+{
+    DataTable dt = createBase();
+
+    if (!string.IsNullOrWhiteSpace(TextBox1.Text))
+    {
+        TextBox1.Text = TextBox1.Text.Trim();
+
+        string searchString = TextBox1.Text.ToLower();
+
+        var sortedRows = dt.AsEnumerable()
+            .Where(row => row.Field<string>("Dolznost").ToLower().Contains(searchString))
+            .OrderByDescending(row => row.Field<string>("Dolznost").Equals(searchString, StringComparison.OrdinalIgnoreCase));
+
+        if (sortedRows.Any())
+        {
+            GridView1.DataSource = sortedRows.CopyToDataTable();
+        }
+        else
+        {
+            GridView1.DataSource = null;
+        }
+
+        GridView1.DataBind();
+    }
+    else
+    {
+        TextBox1.BorderColor = System.Drawing.Color.Red;
+    }
+}
